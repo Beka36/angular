e1cf0fa4f51aef29c70393e038013f453050ae13
Beka36 <137093904+Beka36@users.noreply.github.com>
2024-06-21 13:28:44 +0300

    app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
-import { FormsModule } from "@angular/forms";
+import {NgModule} from "@angular/core";
+import {BrowserModule} from "@angular/platform-browser";
+import {FormsModule} from "@angular/forms";
 import { AppComponent } from "./app.component";
-
+import { BoldDirective } from "./bold.directive";
+import {WhileDirective} from "./while.directive";
+import { DataComponent } from "./data.components";
+import {DataService} from "./data.service";
+import { LogService } from "./log.service";
 @NgModule({
     imports: [BrowserModule, FormsModule],
-    declarations: [AppComponent],
+    declarations: [AppComponent,DataComponent],
+    providers: [DataService,LogService],
     bootstrap: [AppComponent]
 })
-
 export class AppModule {}
