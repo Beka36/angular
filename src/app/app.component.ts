212702f0a1e600eb0b1aaf3632e4da99cd67a4e1
Beka36 <137093904+Beka36@users.noreply.github.com>
2024-06-22 19:39:45 +0300

    app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,22 @@
 import { Component } from "@angular/core";
 
-@Component({
+  @Component({
     selector: 'my-app',
-    template: `<data-comp></data-comp>
-               <data-comp></data-comp>`
-})
-export class AppComponent {}
+    styles:[`
+        .nav{ clear: both;}
+        a {float: left;}
+        .active a {color:red;}
+        `],
+    template: `<div>
+                    <ul class ="nav">
+                   <li routerLinkActive="active" [routerLinkActiveOptions]="{exact:true}">
+                  <a routerLink="">Главная</a>
+                  </li> 
+                  <li routerLinkActive="active">
+                  <a routerLink = "/about">О сайте</a>
+                   </li>
+                   </ul>
+                    <router-outlet></router-outlet>
+    </div>`
+  })
+  export class AppComponent {}
