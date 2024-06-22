3126da50c87ca7ec232289a9ef62d9c0715678ef
Beka36 <137093904+Beka36@users.noreply.github.com>
2024-06-22 19:40:12 +0300

    app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import {NgModule} from "@angular/core";
 import {BrowserModule} from "@angular/platform-browser";
-import {FormsModule} from "@angular/forms";
+import {Routes, RouterModule} from '@angular/router';
 import { AppComponent } from "./app.component";
-import { BoldDirective } from "./bold.directive";
-import {WhileDirective} from "./while.directive";
-import { DataComponent } from "./data.components";
-import {DataService} from "./data.service";
-import { LogService } from "./log.service";
+import { AboutComponent } from "./about.components";
+import { HomeComponent } from "./home.components";
+import { NotFoundComponent } from "./not-found.component";
+
+const appRoutes: Routes =[
+    { path: '', component: HomeComponent},
+    { path: 'about', component: AboutComponent},
+    { path: 'contact', redirectTo: '/about', pathMatch: 'full'},
+    { path: '**', redirectTo: '/'}
+];
+
+
+
+
 @NgModule({
-    imports: [BrowserModule, FormsModule],
-    declarations: [AppComponent,DataComponent],
-    providers: [DataService,LogService],
+    imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
+    declarations: [AppComponent, HomeComponent, AboutComponent, NotFoundComponent],
     bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
